f183c5532f6dc083270802358fd18099355ca958
sahiba0915 <sahibakumari0915@outlook.com>
2025-04-26 23:48:40 +0530

    Initial commit

---
+++ my-portfolio/src/lib/data.ts
@@ -0,0 +1,144 @@
+export const personalInfo = {
+  name: "Sahiba Kumari",
+  location: "Gurugram, India",
+  email: "rkesh2003@gmail.com",
+  github: "https://github.com/sahiba0915",
+  linkedin: "https://www.linkedin.com/in/sahiba0915/",
+};
+
+export const workExperience = [
+  {
+    company: "Clapingo Education Private Limited",
+    location: "Remote",
+    position: "Frontend Development Engineer",
+    period: "Jan 2023 - Present",
+    achievements: [
+      "Developed and maintained responsive, high-performance web applications using React.js and Tailwind CSS, serving 200K+ active users",
+      "Optimized frontend performance by refactoring lines and implementing efficient rendering techniques, reducing,page load times",
+      "Collaborated closely with designers, product owners, and backend developers to ensure seamless integration and functionality",
+    ],
+  },
+];
+
+export const education = [
+  {
+    institution: "Sri Krishna College of Engineering and Technology",
+    location: "Coimbatore, India",
+    degree: "B Tech Information Technology",
+    period: "Jun 2021 - Jun 2025",
+    achievements: [
+      "President of the Department of Information Technology",
+      "G20 student delegate",
+      "Organized multiple technical workshops and events",
+      "Represented my college in multiple national and international hackathons and won.",
+    ],
+  },
+];
+export const skills = {
+  programmingLanguages: [
+    "TypeScript",
+    "JavaScript",
+    "Python",
+    "C++",
+    "Java",
+    "Solidity",
+  ],
+  frontendDevelopment: [
+    "Nextjs",
+    "Reactjs",
+    "React Native",
+    "Shadcn UI",
+    "Tailwind CSS",
+    "HTML",
+    "CSS",
+  ],
+  backendDevelopment: ["Nodejs", "Expressjs"],
+  databaseAndStorage: ["PostgreSQL", "Drizzle (ORM)"],
+  cloudAndDevOps: ["AWS"],
+  toolsAndServices: [
+    "Clerk (Auth)",
+    "Sanity (CMS)",
+    "Tinybird (analytics)",
+    "Zod",
+    "Sentry",
+    "Mixpanel",
+    "Trigger.dev",
+  ],
+};
+
+export const projects = [
+  {
+    title: "Net Zero Carbon Emissions",
+    github: "https://github.com/rishikesh2003/Prodigi",
+    description: [
+      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
+      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
+      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
+      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
+      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
+    ],
+  },
+  {
+    title: "Mental Aarog",
+    github: "https://github.com/rishikesh2003/mental-aarog",
+    description: [
+      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
+      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
+      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
+      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
+      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+    ],
+  },
+];
+
+export const awards = [
+  {
+    name: "IEEE YESIST12 Hackathon",
+    issuer: "IEEE",
+    date: "Sep 2022",
+    type: "International",
+    position: "Second Place",
+  },
+  {
+    name: "Prodigi Cognizant Hackathon",
+    issuer: "Cognizant",
+    date: "Feb 2023",
+    type: "National",
+    position: "Second Runner-up",
+  },
+  {
+    name: "Cisco Thingqbator Hackathon",
+    issuer: "Cisco",
+    date: "Jan 2023",
+    type: "National",
+    position: "First Runner-up",
+  },
+  {
+    name: "Innovators Day",
+    issuer: "Sri Manakula Vinayagar Engineering College, Pondicherry",
+    date: "Sep 2022",
+    type: "National",
+    position: "First Prize",
+  },
+  {
+    name: "KG Hackfest'22",
+    issuer: "KGiSL Institute of Technology, Coimbatore",
+    date: "Sep 2022",
+    type: "National",
+    position: "Second Prize",
+  },
+  {
+    name: "Innohacks'22",
+    issuer: "Innogeeks, KIET Group of Institutions, New Delhi",
+    date: "May 2022",
+    type: "National",
+    position: "Second Runner-up",
+  },
+  {
+    name: "Hack @ SKCET",
+    issuer: "Hackclub SKCET, SKCET, Coimbatore",
+    date: "Feb 2022",
+    type: "National",
+    position: "Most Impactful Hack",
+  },
+];
